a1535b720f4201f48d5aa60e263554d4b22440fa
Olaf Lüke <olaf@tinkerforge.com>
2013-09-16 16:02:04 +0200

    Fix typo

--- software/examples/php/ExampleCallback.php
+++ software/examples/php/ExampleCallback.php
@@ -17,7 +17,7 @@ function cb_reflectivity($reflectivity)
 }
 
 $ipcon = new IPConnection(); // Create IP connection
-$al = new BrickletLine($uid, $ipcon); // Create device object
+$line = new BrickletLine($uid, $ipcon); // Create device object
 
 $ipcon->connect($host, $port); // Connect to brickd
 // Don't use device before ipcon is connected
@@ -25,10 +25,10 @@ $ipcon->connect($host, $port); // Connect to brickd
 // Set Period for reflectivity callback to 1s (1000ms)
 // Note: The reflectivity callback is only called every second if the
 //       reflectivity has changed since the last call!
-$al->setReflectivityCallbackPeriod(1000);
+$line->setReflectivityCallbackPeriod(1000);
 
 // Register reflectivity callback to function cb_reflectivity
-$al->registerCallback(BrickletLine::CALLBACK_REFLECTIVITY, 'cb_reflectivity');
+$line->registerCallback(BrickletLine::CALLBACK_REFLECTIVITY, 'cb_reflectivity');
 
 echo "Press ctrl+c to exit\n";
 $ipcon->dispatchCallbacks(-1); // Dispatch callbacks forever
--- software/examples/php/ExampleThreshold.php
+++ software/examples/php/ExampleThreshold.php
@@ -17,19 +17,19 @@ function cb_reached($reflectivity)
 }
 
 $ipcon = new IPConnection(); // Create IP connection
-$al = new BrickletLine($uid, $ipcon); // Create device object
+$line = new BrickletLine($uid, $ipcon); // Create device object
 
 $ipcon->connect($host, $port); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Get threshold callbacks with a debounce time of 1 second (1000ms)
-$al->setDebouncePeriod(1000);
+$line->setDebouncePeriod(1000);
 
 // Register threshold reached callback to function cb_reached
-$al->registerCallback(BrickletLine::CALLBACK_REFLECTIVITY_REACHED, 'cb_reached');
+$line->registerCallback(BrickletLine::CALLBACK_REFLECTIVITY_REACHED, 'cb_reached');
 
 // Configure threshold for "greater than 2000"
-$al->setReflectivityCallbackThreshold('>', 2000, 0);
+$line->setReflectivityCallbackThreshold('>', 2000, 0);
 
 echo "Press ctrl+c to exit\n";
 $ipcon->dispatchCallbacks(-1); // Dispatch callbacks forever
