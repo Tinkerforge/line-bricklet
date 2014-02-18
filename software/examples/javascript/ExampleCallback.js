1d33c6d3692df0bd9f191c14b6bf55030ec84b10
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-18 16:05:46 +0100

    javascript: examples fixed

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletLine = require('Tinkerforge/BrickletLine');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'j6q';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var line = new BrickletLine(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var line = new Tinkerforge.BrickletLine(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set Period for reflectivity callback to 1s (1000ms)
         // Note: The reflectivity callback is only called every second if the 
@@ -25,7 +24,7 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
 );
 
 // Register reflectivity callback
-line.on(BrickletLine.CALLBACK_REFLECTIVITY,
+line.on(Tinkerforge.BrickletLine.CALLBACK_REFLECTIVITY,
     // Callback function for reflectivity
     function(reflectivity) {
         console.log('Reflectivity: '+reflectivity);
