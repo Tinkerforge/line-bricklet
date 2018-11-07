ec4b1d4e3f1ffbb755b97cdfb85f9b6ce4bf4be9
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:28:02 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for reflectivity events.
-    let reflectivity_receiver = l.get_reflectivity_receiver();
+    let reflectivity_receiver = l.get_reflectivity_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `l` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `l` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for reflectivity in reflectivity_receiver {
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Get threshold receivers with a debounce time of 1 second (1000ms).
     l.set_debounce_period(1000);
 
-    // Create receiver for reflectivity reached events.
-    let reflectivity_reached_receiver = l.get_reflectivity_reached_receiver();
+    let reflectivity_reached_receiver = l.get_reflectivity_reached_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `l` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `l` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for reflectivity_reached in reflectivity_reached_receiver {
