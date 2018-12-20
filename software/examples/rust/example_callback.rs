93891f6b6249f6c0e47b756b85e2ef69077e7953
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:56:19 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  line_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, line_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,23 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let l = LineBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let reflectivity_receiver = l.get_reflectivity_callback_receiver();
+    let reflectivity_receiver = l.get_reflectivity_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `l` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for reflectivity in reflectivity_receiver {           
-                		println!("Reflectivity: {}", reflectivity);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `l` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for reflectivity in reflectivity_receiver {
+            println!("Reflectivity: {}", reflectivity);
+        }
+    });
 
-		// Set period for reflectivity receiver to 1s (1000ms).
-		// Note: The reflectivity callback is only called every second
-		//       if the reflectivity has changed since the last call!
-		l.set_reflectivity_callback_period(1000);
+    // Set period for reflectivity receiver to 1s (1000ms).
+    // Note: The reflectivity callback is only called every second
+    //       if the reflectivity has changed since the last call!
+    l.set_reflectivity_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  line_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, line_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,24 +10,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let l = LineBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get threshold receivers with a debounce time of 1 second (1000ms).
-		l.set_debounce_period(1000);
+    // Get threshold receivers with a debounce time of 1 second (1000ms).
+    l.set_debounce_period(1000);
 
-     let reflectivity_reached_receiver = l.get_reflectivity_reached_callback_receiver();
+    let reflectivity_reached_receiver = l.get_reflectivity_reached_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `l` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for reflectivity_reached in reflectivity_reached_receiver {           
-                		println!("Reflectivity: {}", reflectivity_reached);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `l` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for reflectivity_reached in reflectivity_reached_receiver {
+            println!("Reflectivity: {}", reflectivity_reached);
+        }
+    });
 
-		// Configure threshold for reflectivity "greater than 2000".
-		l.set_reflectivity_callback_threshold('>', 2000, 0);
+    // Configure threshold for reflectivity "greater than 2000".
+    l.set_reflectivity_callback_threshold('>', 2000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
